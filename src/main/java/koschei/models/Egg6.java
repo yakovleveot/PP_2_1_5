4c4c91c62026bdf96accd0348bd61e69ca115515
yakovleveot <yakovlev.eot@ya.ru>
2023-02-11 20:17:45 +0300

    Initial commit

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,17 @@
 package koschei.models;
 
-public class Egg6 {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
+public class Egg6 {
+    @Autowired
+    private final Needle7 needle;
+    public Egg6(Needle7 needle) {
+        this.needle = needle;
+    }
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + needle.toString();
     }
 }
--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,15 @@
 package koschei.models;
 
-public class Needle7 {
+import org.springframework.stereotype.Component;
 
+@Component
+public class Needle7 {
+    private final Deth8 deth;
+    public Needle7(Deth8 deth) {
+        this.deth = deth;
+    }
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле :( " + deth.toString();
     }
 }
